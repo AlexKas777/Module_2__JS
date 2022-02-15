2492ab9b961ee485f7751c94cb9a38ae6fa59cf0
AlexKas777 <alexkas007983@gmail.com>
2022-02-15 17:48:13 +0200

    Done ---> Additional# 6 (Added alphabetical sorting)

--- 6_les/Additional/main.js
+++ 6_les/Additional/main.js
@@ -71,81 +71,187 @@
 // mailValidator('some.email@gmail.com')
 
 
-
-
 // @@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-
 // @@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-@@@@@@-()-
 
 // - є масив
-// let coursesArray = [
-//     {
-//         title: 'JavaScript Complex',
-//         monthDuration: 5,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
-//     },
-//     {
-//         title: 'Java Complex',
-//         monthDuration: 6,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'git',
-//             'java core',
-//             'java advanced']
-//     },
-//     {
-//         title: 'Python Complex',
-//         monthDuration: 6,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'python core',
-//             'python advanced']
-//     },
-//     {
-//         title: 'QA Complex',
-//         monthDuration: 4,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
-//     },
-//     {
-//         title: 'FullStack',
-//         monthDuration: 7,
-//         hourDuration: 909,
-//         modules: ['html',
-//             'css',
-//             'js',
-//             'mysql',
-//             'mongodb',
-//             'react',
-//             'angular',
-//             'aws',
-//             'docker',
-//             'git',
-//             'node.js',
-//             'python',
-//             'java']
-//     },
-//     {
-//         title: 'Frontend',
-//         monthDuration: 4,
-//         hourDuration: 909,
-//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
-//     }
-// ];
+// debugger
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html',]
+    },
+    {
+        title: 'Html Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css',]
+    },
+    {
+        title: 'Js Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Mysql Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Mongodb Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'React Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Angular Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Aws Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Docker Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Git Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Node.js Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Core java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Advanced Java  Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+// // // @@@@@@@ New @@@@@@@
+// відсортувати його по алфавіту
+
+let sortCoursesArrayABC = coursesArray.sort((a,b)=>{
+    if (a.title<b.title){
+        return -1
+    } else if (a.title>b.title){
+        return 1
+    } else if (a.title === b.title){
+        return 0
+    }
+})
+console.log(sortCoursesArrayABC);
+
+
 
 // відсортувати його в спадаючому порядку за кількістю елементів в полі modules
 
